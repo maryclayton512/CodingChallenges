4c7ebff595c856653ac985a5cb794d113f6573a2
Mary Clayton <mclaytn15@gmail.com>
2022-07-04 17:50:59 -0700

    added first program data

---
+++ firstprogram.py
@@ -0,0 +1,15 @@
+#Name: Mary Clayton
+#Date: 7/4/2022
+#Purpose: Discussion 1
+
+#INPUT
+#Input name, age, and favorite color
+name= input("Please enter your name: ")
+
+age = int(input("Please enter your age: "))
+
+color = input("Please enter your favorite color: ")
+
+#OUTPUT
+#Output welcome message
+print("Welcome", name,"! Your age is", age, "and your favorite color is", color,".")
